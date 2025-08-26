7ccf509ed5510234a18eaf3e7ce7fda5585281b8
Clayton Rocha <clayton.will08@gmail.com>
2025-08-26 03:38:03 -0300

    Criação de paginas para receber dados via api

--- js/script.js
+++ js/script.js
@@ -20,7 +20,6 @@ async function getSimulatedExam() {
       tdNome.textContent = item.nome;
       tr.appendChild(tdNome);
 
-
       const tdDescricao = document.createElement('td');
       tdDescricao.textContent = item.descricao;
       tr.appendChild(tdDescricao);
@@ -34,11 +33,18 @@ async function getSimulatedExam() {
       tr.appendChild(tdDeletar)
 
       tabela.appendChild(tr);
+    });
 
-      const teste = document.getElementById("opcao")
-      teste.textContent = item.nome
+    const container = document.getElementById("container");
 
-      console.log(teste)
+    dados.forEach(item => {
+      const select = document.createElement("select");
+
+      const op = document.createElement("option");
+      op.textContent = item.nome;
+      select.appendChild(op);
+
+      container.appendChild(select);
     });
 
   } catch (error) {
@@ -67,8 +73,12 @@ async function getQuestionExam() {
       tr.appendChild(tdNome);
 
       const tdDescricao = document.createElement('td');
-      tdDescricao.textContent = item.descricao;
+      tdDescricao.textContent = item.explicacao;
       tr.appendChild(tdDescricao);
+
+      const tdNomeExame = document.createElement('td');
+      tdNomeExame.textContent = item.nomeExame;
+      tr.appendChild(tdNomeExame);
 
       const tdEditar = document.createElement('td');
       tdEditar.innerHTML = `<button onclick="editar(${item.id})">Editar</button>`;
@@ -81,40 +91,108 @@ async function getQuestionExam() {
       tabela.appendChild(tr);
     });
 
+
   } catch (error) {
     console.error('Erro ao carregar dados:', error);
   }
 }
 
-document.getElementById("createSimulatedExam").addEventListener("submit", function (event) {
-  event.preventDefault();
+async function getOption() {
+  try {
+    const endpoint = "/api/Option";
+    const response = await fetch(`${apiBase}${endpoint}`);
+    const dados = await response.json();
+    console.log(dados);
 
-  const data = {
-    nome: document.getElementById("nome").value,
-    descricao: document.getElementById("descricao").value
-  };
+    const tabela = document.getElementById('tbOption');
 
-  const endpoint = "/api/SimulatedExam";
+    dados.forEach(item => {
+      const tr = document.createElement('tr');
 
-  fetch((`${apiBase}${endpoint}`), {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  })
-    .then(res => res.json())
-    .then(result => console.log("Resposta da API:", result))
-    .catch(err => console.error("Erro:", err));
-});
+      const tdId = document.createElement('td');
+      tdId.textContent = item.id;
+      tr.appendChild(tdId);
 
-document.querySelectorAll(".editar").forEach(btn => {
-  btn.addEventListener("click", e => {
-    const id = e.target.dataset.id;
-    alert("Editar id: " + id);
-    console.log(id)
+      const tdNome = document.createElement('td');
+      tdNome.textContent = item.texto;
+      tr.appendChild(tdNome);
+
+      const tdDescricao = document.createElement('td');
+      tdDescricao.textContent = item.correta;
+      tr.appendChild(tdDescricao);
+
+      const tdNomeExame = document.createElement('td');
+      tdNomeExame.textContent = item.questao;
+      tr.appendChild(tdNomeExame);
+
+      const tdEditar = document.createElement('td');
+      tdEditar.innerHTML = `<button onclick="editar(${item.id})">Editar</button>`;
+      tr.appendChild(tdEditar)
+
+      const tdDeletar = document.createElement('td');
+      tdDeletar.innerHTML = `<button onclick="deletar(${item.id})">Deletar</button>`;
+      tr.appendChild(tdDeletar)
+
+      tabela.appendChild(tr);
+    });
+
+
+  } catch (error) {
+    console.error('Erro ao carregar dados:', error);
+  }
+}
+
+function postSimulatedExam() {
+  document.getElementById("createSimulatedExam").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const data = {
+      nome: document.getElementById("nome").value,
+      descricao: document.getElementById("descricao").value
+    };
+
+    const endpoint = "/api/SimulatedExam";
+
+    fetch((`${apiBase}${endpoint}`), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(result => console.log("Resposta da API:", result))
+      .catch(err => console.error("Erro:", err));
   });
-});
+}
+
+function postQuestion() {
+  document.getElementById("createQuestion").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const data = {
+      texto: document.getElementById("enunciado").value,
+      explicacao: document.getElementById("explicacao").value,
+      examId: document.getElementById("selecao").value,
+      ordem: 1
+
+    };
+    console.log(data.examId)
+
+    const endpoint = "/api/question";
+
+    fetch((`${apiBase}${endpoint}`), {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(result => console.log("Resposta da API:", result))
+      .catch(err => console.error("Erro:", err));
+  });
+}
 
 async function editar(id) {
   try {
@@ -154,4 +232,28 @@ async function deletar(id) {
 
 if (document.getElementById('tbSimulados') != null) getSimulatedExam();
 
+if (document.getElementById('tbOption') != null) getOption();
+
 if (document.getElementById('tbQuestion') != null) getQuestionExam();
+
+if (document.getElementById('createSimulatedExam') != null) postSimulatedExam();
+
+if (document.getElementById('createQuestion') != null) getSimulatedExamNome(), postQuestion();
+
+async function getSimulatedExamNome() {
+  const endpoint = "/api/SimulatedExam";
+  const response = await fetch(`${apiBase}${endpoint}`);
+  const dados = await response.json();
+  console.log(dados);
+
+  const select = document.getElementById('selecao')
+
+  dados.forEach(item => {
+    const op = document.createElement('option')
+    op.textContent = item.nome
+    op.value = item.id
+    select.appendChild(op);
+  });
+}
+
+
